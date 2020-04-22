28808fdc41f887c6f1141c946077976b5d69791d
weichichou <weichi.cc@gmail.com>
2020-04-22 15:23:44 +0200

    Provision

--- example.tf
+++ example.tf
@@ -6,9 +6,13 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami ="ami-b374d5a5"
     instance_type="t2.micro"
+
+    provisioner "local-exec"{
+        command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+    }
 }
 
-resource "aws_eip" "ip" {
-  vpc=true
-  instance = aws_instance.example.id
-}
+# resource "aws_eip" "ip" {
+#   vpc=true
+#   instance = aws_instance.example.id
+# }
